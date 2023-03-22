96a22915692a9b69e3e9998751ac9a2a9e1c169f
amadayami <amadayamichael@gmail.com>
2023-03-22 09:55:39 -0600

    removed hard coded messages from feed. edited seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(username: "Amelia", password: "stinkybutt")
-User.create(username: "Marty", password: "furpants")
+#User.create(username: "Amelia", password: "stinkybutt")
+#User.create(username: "Marty", password: "furpants")
