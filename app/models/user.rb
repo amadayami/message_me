1ffbb6630b501828d393315ea3d51271c3501532
amadayami <amadayamichael@gmail.com>
2023-03-22 08:56:29 -0600

    generated messages. updated message model and table. updated user model to create association with messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false },
                       length: { minimum: 3, maximum: 20 }
-  has_secure_password
+has_many :messages
+has_secure_password
 end
