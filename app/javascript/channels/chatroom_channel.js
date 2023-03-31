0d2e8d8c2546434a62e97e03b4ddd91988ffe4a1
amadayami <amadayamichael@gmail.com>
2023-03-31 17:20:36 -0600

    fixed issue with pin syntax. added alert for testing. updated messages_controller.rb to use ActionCable.

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,5 +11,8 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    (function() {
+      alert(data.foo);
+    }).call(this);
   }
 });
