ba5c85159e93b6cbf3c47ab1b256d1807feb0b41
amadayami <amadayamichael@gmail.com>
2023-04-02 16:04:17 -0600

    messages updated in real time using message partial.

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,8 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    (function() {
-      alert(data.foo);
-    }).call(this);
+    $('#message-container').append(data["mod_message"]);
   }
 });
